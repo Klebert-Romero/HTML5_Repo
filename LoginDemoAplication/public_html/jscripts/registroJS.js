6a86bc4410e3a15efbdbcc2d235d2d643e923b36
Administrador <Administrador@DESKTOP-5E85M15>
2019-11-21 22:10:52 -0500

    Segundo Commit

--- LoginDemoAplication/public_html/jscripts/registroJS.js
+++ LoginDemoAplication/public_html/jscripts/registroJS.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
         var mes = $("#mes").val();
         var genero = $("#genero").val();
         var telefono = $("#telefono").val();
-        
-    })
-})
+        if(clave.length < 6){
+            alert("La clave deberia tener mínimo 6 cracteres");
+            return false;
+        }
+        if(mes == ''){
+            alert("Seleccione mes váido");
+            return false;
+        }
+        if(genero == ''){
+            alert("Seleccione genero");
+            return false;
+        }
+        if(telefono.length < 8){
+            alert("Ingrese de celular numero valido");
+            return false;
+        }
+        alert ("Validaciones OK");
+    });
+});
